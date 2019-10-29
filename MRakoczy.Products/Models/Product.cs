c7a998e37c8acee39cc74f452b9cd744296205b9
mrakoczy <mrakoczy@umbitious.com>
2019-10-29 11:00:47 +0100

    Add Product model

---
+++ MRakoczy.Products/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MRakoczy.Products.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(200)")]
+        public string Name { get; set; }
+        
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
