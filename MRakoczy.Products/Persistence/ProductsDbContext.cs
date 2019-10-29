5fe18ea61840423f4eb2889e6d4af5048a60e2e1
mrakoczy <mrakoczy@umbitious.com>
2019-10-29 11:34:01 +0100

    Add DBContext

---
+++ MRakoczy.Products/Persistence/ProductsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MRakoczy.Products.Models;
+
+namespace MRakoczy.Products.Persistence
+{
+    public class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
