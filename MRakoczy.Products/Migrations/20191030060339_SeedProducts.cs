4a5371db5ccacf3d09178dfe50aeee6818c9c736
mrakoczy <mrakoczy@umbitious.com>
2019-10-30 07:08:58 +0100

    Seed Products

---
+++ MRakoczy.Products/Migrations/20191030060339_SeedProducts.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MRakoczy.Application.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products (Name, Price) VALUES ('Tomato', 1.00)");
+            migrationBuilder.Sql("INSERT INTO Products (Name, Price) VALUES ('Beetroot', 2.00)");
+            migrationBuilder.Sql("INSERT INTO Products (Name, Price) VALUES ('Garlic', 2.40)");
+            migrationBuilder.Sql("INSERT INTO Products (Name, Price) VALUES ('Fungi', 3.11)");
+            migrationBuilder.Sql("INSERT INTO Products (Name, Price) VALUES ('Pumpkin', 1.20)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
